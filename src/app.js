104c4a403f0282efee4985fbf3eb1ad6b0b7809f
Joanna <askat58@gmail.com>
2021-01-24 20:19:49 +0000

    change relevant icons
    use OpenWeather icons now

--- src/app.js
+++ src/app.js
@@ -49,6 +49,9 @@ function displayWeather(response) {
   document.querySelector("#city-humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#temp").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#description").innerHTML = response.data.weather[0].main;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);ś
 }
 
 function searchCity(city) {
